3fed59fce4b993e8ef1186b007d1eebfc71d4347
Mostafa12-sayed <ms2413@fayoum.edu.eg>
2025-03-09 17:13:02 +0200

    add tables jobs and applications table migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('role', ['employer', 'ucandidateser']);
+            $table->string('phone_number', 15)->nullable();
+            $table->string('profile_picture')->nullable();
             $table->timestamps();
         });
 
