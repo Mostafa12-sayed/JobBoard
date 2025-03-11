ec73abe911af6afd62f99b639130ec7580959766
HadyAbdelmonem <hadyabdelmonem11@gmail.com>
2025-03-11 14:10:41 +0200

    add breeze

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
 
-use App\Http\Controllers\Dashboard\CategoryController;
-use App\Http\Controllers\Dashboard\UsersController;
-use App\Http\Controllers\Dashboard\AuthController;
-use App\Http\Controllers\Dashboard\DashboardController;
-use App\Http\Controllers\Dashboard\JobsAdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('Website.jobs');
-// });
-
-Route::group(['middleware' => 'guest:admin', 'prefix' => 'dashboard', 'as' => 'dashboard'], function () {
-    Route::get('/login', [AuthController::class, 'view'])->name('.login');
-    Route::post('/login',  [AuthController::class, 'login'])->name('.login.store');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::group(['middleware' => 'auth.admin', 'prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('index');
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::resource('category', CategoryController::class)->names('category')->except(['destroy', 'show']);
-    Route::get('/category/destroy/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
-    Route::post('/category/chanageStatus', [CategoryController::class, 'changeStatus'])->name('category.changeStatus');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::get('/jobs', [JobsAdminController::class, 'index'])->name('jobs.index');
-    Route::get('/jobs/{job}', [JobsAdminController::class, 'show'])->name('jobs.show');
-    Route::post('/jobs/changeStatus', [JobsAdminController::class, 'changeStatus'])->name('jobs.changeStatus');
-
-
-    Route::resource('users', UsersController::class)->names('user')->except(['destroy']);
-    Route::get('/users/destroy/{user}', [UsersController::class, 'destroy'])->name('user.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
